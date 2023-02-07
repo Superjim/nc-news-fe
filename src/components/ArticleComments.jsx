7b04179a8887b2f29ce6921ca6a92a6aa1abced2
Jim Jenkinson <jamesjenkinson10@gmail.com>
2023-02-07 10:44:20 +0000

    added comments

---
+++ src/components/ArticleComments.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ArticleComments() {
+  return <div>ArticleComments</div>;
+}
+
+export default ArticleComments;
---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { AiOutlineUser } from "react-icons/ai";
+import {
+  BsFillArrowUpCircleFill,
+  BsFillArrowDownCircleFill,
+} from "react-icons/bs";
+
+function Comment({ props }) {
+  const { comment_id, votes, created_at, author, body, article_id } = props;
+
+  return (
+    <div className="comment-container">
+      <span className="comment-user-container">
+        <AiOutlineUser size={32} />
+      </span>
+      <div className="comment-body-container">
+        <span className="comment-title-container">
+          <h5>{author}</h5>
+          <h5>{created_at}</h5>
+          <h5># {comment_id}</h5>
+        </span>
+        <p>{body}</p>
+        <span className="comment-vote-container">
+          <BsFillArrowUpCircleFill
+            size={32}
+            onClick={() => console.log("upvote")}
+          />
+          <p>{votes}</p>
+          <BsFillArrowDownCircleFill
+            size={32}
+            onClick={() => console.log("downvote")}
+          />
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Comment;

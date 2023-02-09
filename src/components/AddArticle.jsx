ca7c5a76f05d45753511769e1c2c750c1e099559
Jim Jenkinson <jamesjenkinson10@gmail.com>
2023-02-09 12:41:02 +0000

    added add new article functionality

--- src/components/AddArticle.jsx
+++ src/components/AddArticle.jsx
@@ -4,8 +4,8 @@ import { ArticleContext } from "../contexts/ArticleContext";
 import { api } from "../utils/api";
 import Article from "./Article";
 
-function AddArticle({ topic }) {
-  const { topics } = useContext(ArticleContext);
+function AddArticle() {
+  const { topics, checkedTopics } = useContext(ArticleContext);
   const { user } = useContext(UserContext);
   const [form, setForm] = useState({
     author: user.username,
@@ -37,71 +37,91 @@ function AddArticle({ topic }) {
 
   return (
     <div className="content">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Title:</label>
-          <input
-            placeholder="Enter a title for your new article"
-            type="text"
-            id="title"
-            name="title"
-            value={form.title}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="body">Body:</label>
-          <textarea
-            placeholder="Write your article here"
-            id="body"
-            name="body"
-            value={form.body}
-            onChange={handleChange}
-            required
-          />
-        </div>
+      {page === 2 && (
+        <Article
+          props={{
+            ...form,
+            article_id: -202,
+            created_at: new Date().getTime(),
+            votes: 0,
+          }}
+          showAll={true}
+        />
+      )}
+      <div className="add-article-container">
+        <form className="add-article-form" onSubmit={handleSubmit}>
+          {page === 1 && (
+            <div className="add-article-input-container">
+              <label htmlFor="title">
+                <h3>Title</h3>
+              </label>
+              <input
+                placeholder="Enter a title for your new article"
+                type="text"
+                id="title"
+                name="title"
+                value={form.title}
+                onChange={handleChange}
+                required
+              />
+              <label htmlFor="body">
+                <h3>Body</h3>
+              </label>
+              <textarea
+                placeholder="Write your article here"
+                id="body"
+                name="body"
+                value={form.body}
+                onChange={handleChange}
+                required
+              />
+              <label htmlFor="topic">
+                <h3>Topic</h3>
+              </label>
+              <select
+                id="topic"
+                name="topic"
+                value={form.topic}
+                onChange={handleChange}
+                required
+              >
+                <option>Choose Topic</option>
+                {topics.map((topic, index) => (
+                  <option key={index} value={topic.slug}>
+                    {topic.slug}
+                  </option>
+                ))}
+              </select>
+              <label htmlFor="article_img_url">
+                <h3>Article Image URL</h3>
+              </label>
+              <input
+                type="url"
+                id="article_img_url"
+                name="article_img_url"
+                value={form.article_img_url}
+                onChange={handleChange}
+              />
+              <span className="add-article-button-container">
+                <button disabled>Prev</button>
+                <button type="button" onClick={() => setPage(2)}>
+                  Next
+                </button>
+              </span>
+            </div>
+          )}
 
-        <div>
-          <label htmlFor="topic">Topic:</label>
-          <select
-            id="topic"
-            name="topic"
-            value={form.topic}
-            onChange={handleChange}
-            required
-          >
-            <option>Choose Topic</option>
-            {topics.map((topic, index) => (
-              <option key={index} value={topic.slug}>
-                {topic.slug}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div>
-          <label htmlFor="article_img_url">Article Image URL:</label>
-          <input
-            type="url"
-            id="article_img_url"
-            name="article_img_url"
-            value={form.article_img_url}
-            onChange={handleChange}
-          />
-        </div>
-
-        <button type="submit">Submit</button>
-      </form>
-      <Article
-        props={{
-          ...form,
-          article_id: -202,
-          created_at: new Date().getTime(),
-          votes: 0,
-        }}
-        showAll={true}
-      />
+          {page === 2 && (
+            <div className="add-article-input-container">
+              <h3>Happy with how your article looks?</h3>
+              <span className="add-article-button-container">
+                <button onClick={() => setPage(1)}>Prev</button>
+                <button type="submit">Post Article</button>
+              </span>
+            </div>
+          )}
+        </form>
+      </div>
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import NavBarTopic from "./NavBarTopic";
 import SortBarArticles from "./SortBarArticles";
 
@@ -8,6 +9,9 @@ function Sidebar() {
       <div className="sidebar-container">
         <NavBarTopic />
         <SortBarArticles />
+        <Link to="/add-article">
+          <h2>Add new article</h2>
+        </Link>
       </div>
     </div>
   );

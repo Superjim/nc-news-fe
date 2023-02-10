5a836aa9cc1569a1132f0da6d14dc1922e5eecd2
Jim Jenkinson <jamesjenkinson10@gmail.com>
2023-02-10 13:17:48 +0000

    Built mobile view, made improvements for small screen sizes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import NavBarTopic from "./NavBarTopic";
 import SortBarArticles from "./SortBarArticles";
@@ -6,32 +6,41 @@ import { ArticleContext } from "../contexts/ArticleContext";
 
 function Sidebar() {
   const { setCheckedTopics, setNavbarAll } = useContext(ArticleContext);
+  const [visible, setVisible] = useState(true);
 
   return (
     <div className="sidebar">
-      <div className="sidebar-container">
-        <NavBarTopic />
-        <SortBarArticles />
-        <span className="add-container">
-          <Link
-            onClick={() => {
-              setCheckedTopics([]);
-              setNavbarAll(true);
-            }}
-            to="/"
-          >
-            <button className="generic-button">Back to All</button>
-          </Link>
-          <br></br>
-          <Link to="/add-article">
-            <button className="generic-button">Add new article</button>
-          </Link>
-          <br></br>
-          <Link to="/add-topic">
-            <button className="generic-button">Add new topic</button>
-          </Link>
-        </span>
-      </div>
+      <button
+        onClick={() => setVisible(!visible)}
+        className="generic-button sidebar-button"
+      >
+        Toggle Sidebar
+      </button>
+      {visible && (
+        <div className="sidebar-container">
+          <NavBarTopic />
+          <SortBarArticles />
+          <span className="add-container">
+            <Link
+              onClick={() => {
+                setCheckedTopics([]);
+                setNavbarAll(true);
+              }}
+              to="/"
+            >
+              <button className="generic-button">Back to All</button>
+            </Link>
+            <br></br>
+            <Link to="/add-article">
+              <button className="generic-button">Add new article</button>
+            </Link>
+            <br></br>
+            <Link to="/add-topic">
+              <button className="generic-button">Add new topic</button>
+            </Link>
+          </span>
+        </div>
+      )}
     </div>
   );
 }

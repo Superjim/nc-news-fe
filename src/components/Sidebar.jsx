413f74d4a00ee9af80d136d3122952a30040bcb2
Jim Jenkinson <jamesjenkinson10@gmail.com>
2023-02-10 11:51:23 +0000

    improved sidebar layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import NavBarTopic from "./NavBarTopic";
 import SortBarArticles from "./SortBarArticles";
+import { ArticleContext } from "../contexts/ArticleContext";
 
 function Sidebar() {
+  const { setCheckedTopics, setNavbarAll } = useContext(ArticleContext);
+
   return (
     <div className="sidebar">
       <div className="sidebar-container">
         <NavBarTopic />
         <SortBarArticles />
-        <Link to="/add-article">
-          <h2>Add new article</h2>
-        </Link>
-        <Link to="/add-topic">
-          <h2>Add new topic</h2>
-        </Link>
+        <span className="add-container">
+          <Link
+            onClick={() => {
+              setCheckedTopics([]);
+              setNavbarAll(true);
+            }}
+            to="/"
+          >
+            <button className="generic-button">Back to All</button>
+          </Link>
+          <br></br>
+          <Link to="/add-article">
+            <button className="generic-button">Add new article</button>
+          </Link>
+          <br></br>
+          <Link to="/add-topic">
+            <button className="generic-button">Add new topic</button>
+          </Link>
+        </span>
       </div>
     </div>
   );

8d7873282d6f057fd595c0d0896446dc9fd72f8f
Jim Jenkinson <jamesjenkinson10@gmail.com>
2023-02-12 01:39:43 +0000

    fixed pressing back in the browser or on the keyboard doesnt change the sidebar

--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -6,13 +6,27 @@ import Article from "./Article";
 function Topic() {
   let { topic_slug } = useParams();
 
-  const { articles, fetchArticles, checkedTopics, sortBy, order, limit, page } =
-    useContext(ArticleContext);
+  const {
+    articles,
+    fetchArticles,
+    checkedTopics,
+    sortBy,
+    order,
+    limit,
+    page,
+    setNavbarAll,
+  } = useContext(ArticleContext);
 
   //this useEffect gets the articles, so only values that change the api query should be in here
   //if you add the recommended stuff it'll get itself stuck in a topic_slug loop or 10 and isnt very performant
   //basically DONT ADD TOPIC_SLUG BECAUSE IT WILL RUN EVERY RENDER
   useEffect(() => {
+    //this SHOULD set the sidebar to all if theres no params, ie if the user presses back to return from a single topic to the all page
+    if (topic_slug === undefined) {
+      setNavbarAll(true);
+    } else {
+      setNavbarAll(false);
+    }
     fetchArticles(topic_slug);
   }, [checkedTopics, sortBy, order, limit, page, articles]);
 
